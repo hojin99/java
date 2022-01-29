9cfb1d39713f1f2d687498c98a92716894a60be3
조호진 <hojin@innowireless.co.kr>
2022-01-29 12:03:23 +0900

    lamda

--- src/lamda/Lamda.java
+++ src/lamda/Lamda.java
@@ -9,18 +9,26 @@ public class Lamda {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("square(5) : " + square(5));
-        
+        // 람다식 사용
         MyLamda sqr = x -> x*x;
         System.out.println("lamda : " + sqr.mySquare(5));
 
         // 람다 함수를 리턴해서 이어서 함수 호출이 가능
         returnLamda().showSomething("Hello! ");
 
-    }
+        // 기존 스레드 실행
+        new Thread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread run");
+                }
+            }
+        ).start();
+        
+        // 람다식으로 스레드 실행
+        new Thread(() -> System.out.println("Thread run ramda")).start();
 
-    static int square(int x) {
-        return x*x;
     }
 
     static MyPrint returnLamda() {
@@ -29,6 +37,9 @@ public class Lamda {
     
 }
 
+// 함수형 인터페이스 (단일 메소드를 가짐, 람다식은 단일 메소드 인터페이스여야 함)
+// 어노테이션은 함수형 인터페이스를 확실하기 위함 (컴파일 에러), 없어도 단일 메소드라면 동작은 함
+@FunctionalInterface
 interface MyLamda {
     int mySquare(int x);
 }
